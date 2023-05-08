ff789fc15ff653ab0d8a6743fe0c9dfc479f93ba
suopsuop <theguywhohasnoloaf@gmail.com>
2023-05-08 12:10:05 +1000

    Implementing timer

---
+++ ProjectThanatos2/Content/Source/Timer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using ProjectThanatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectThanatos2.Content.Source
+{
+    internal class Timer
+    {
+        public double timerTime = 0;
+        public double delayTime = 0;
+
+        GameTime gameTime = ProjectThanatos.ProjectThanatos.GameTime;
+
+        public Timer(double delayTime)
+        {
+            this.delayTime = delayTime;
+        }
+
+        public void Wait(double delayTime, Action action)
+        {
+
+        }
+    }
+}
